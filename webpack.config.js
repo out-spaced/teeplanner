e2ef596ffcb9a7bb4240766eabd9e0e41422f9d8
Alex Tarnawski <amtarnawski@gmail.com>
2021-07-07 12:32:42 -0700

    created skeleton

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const path = require('path');
+
+module.exports = {
+  watch: true,
+  entry: [
+    path.resolve(__dirname, 'src', 'index.jsx'),
+  ],
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'public'),
+  },
+  mode: 'development',
+  module: {
+    rules: [
+      {
+        test: [/\.js$/, /\.jsx?$/],
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+}
